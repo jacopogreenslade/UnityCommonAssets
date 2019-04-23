5356f4da4d48b91449f2415d5126690eebcef881
Jacopo Greenslade <jacopo.greenslade@gmail.com>
2019-04-23 11:33:17 -0400

    Added Event/Response triggers and meta files

---
+++ Events/TriggerEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author: Jacopo Greenslade
+public class TriggerEvent : MonoBehaviour {
+    public GameEvent g;
+  
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (g)
+            g.Raise();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (g)
+            g.Raise();
+    }
+}
---
+++ Events/TriggerResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author: Jacopo Greenslade
+public class TriggerResponse : MonoBehaviour {
+
+    public AbstractResponse r;
+
+    public string checkForTag;
+  
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (checkForTag != "" && other.CompareTag(checkForTag))
+        {
+            r.InvokeResponse(this);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (checkForTag != "" && other.CompareTag(checkForTag))
+        {
+            r.InvokeResponse(this);
+        }
+    }
+}
