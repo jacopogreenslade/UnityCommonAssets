7ab0c9cd453f8a35ca1016dd245255311f390dc8
Jacopo Greenslade <jacopo.greenslade@gmail.com>
2019-04-24 11:43:05 -0400

    Added object pooling

---
+++ Pooling/ObjectPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author: Jacopo Greenslade
+// Why not make it static and just make the original objects responsible for the pools
+public class ObjectPool {
+
+    private List<GameObject> pool;
+
+    // Store this so you can instantiate objects if necessary
+    private GameObject o;
+
+    public void PopulatePool(MonoBehaviour caller, int poolSize, GameObject obj)
+    {
+        pool = new List<GameObject>();
+        // Store object for later use
+        o = obj;
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            pool.Add(CreateObject(obj));
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        foreach (var o in pool)
+        {
+            if (!o.activeInHierarchy)
+            {
+                return o;
+            }
+        }
+        var go = CreateObject(o);
+        pool.Add(go);
+        return go;
+    }
+
+    private GameObject CreateObject(GameObject obj)
+    {
+        var go = Object.Instantiate(obj);
+        go.SetActive(false);
+        return go;
+    }
+}
